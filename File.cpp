7895f43a2189733b00fe6d75fad3c5b973a9930f
JialongYin <yinjlusa@gmail.com>
2022-03-03 01:57:52 +0800

    implement encapsulation

--- File.cpp
+++ File.cpp
@@ -17,7 +17,7 @@ void File::diff(File &file, const std::string &fileOutput) {
         std::getline(file.filept_, line);
         // filept_.getline(line_, 256);
         // file.filept_.getline(line, 256);
-        // std::cout << line << " " << typeid(line).name() << std::endl;
+        std::cout << line << " " << typeid(line).name() << std::endl;
         out << compare_(line_, line) << std::endl;
         // out << compare_(std::string(line_), std::string(line)) << std::endl;
 
