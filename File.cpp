e2d32fa451e207340515b7b2090fbe2e909f9c08
JialongYin <yinjlusa@gmail.com>
2022-03-03 02:17:18 +0800

    implement encapsulation

--- File.cpp
+++ File.cpp
@@ -5,14 +5,14 @@ File::File(std::string filename): filept_(filename), compare_(Diff()) {
     getline(filept_, type_);
     int pos = type_.find('_');
     compare_ = Diff(type_.substr(0, pos));
-    std::cout << "File(): " << type_ << typeid(type_).name() << (typeid(type_) == typeid(std::string)) << std::endl;
+    // std::cout << "File(): " << type_ << typeid(type_).name() << (typeid(type_) == typeid(std::string)) << std::endl;
 }
 
 void File::diff(File &file, const std::string &fileOutput) {
-    this->setTobegin();
-    std::string temp;
-    std::getline(filept_, temp);
-    std::cout << "temp: " << temp << typeid(temp).name() << std::endl;
+    // this->setTobegin();
+    // std::string temp;
+    // std::getline(filept_, temp);
+    // std::cout << "temp: " << temp << typeid(temp).name() << std::endl;
 
 
     std::ofstream out(fileOutput);
@@ -21,8 +21,8 @@ void File::diff(File &file, const std::string &fileOutput) {
     while (!filept_.eof()) {
         std::getline(filept_, line_);
         std::getline(file.filept_, line);
-        std::cout << "line_: " << line_ << typeid(line_).name() << std::endl;
-        std::cout << "line: " << line << typeid(line).name() << std::endl;
+        std::cout << "line_: " << line_ << " " << typeid(line_).name() << std::endl;
+        std::cout << "line: " << line << " " << typeid(line).name() << std::endl;
         std::cout << "compare_: " << compare_(line_, line) << std::endl;
         // filept_.getline(line_, 256);
         // file.filept_.getline(line, 256);
